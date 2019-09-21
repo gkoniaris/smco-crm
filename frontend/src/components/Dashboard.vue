cdaf238abc0812b43a29696d9d7b76fa5f8d4bce
George Koniaris <koniaris.giwrgos@gmail.com>
2019-09-21 12:40:57 +0300

    Theming

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -3,7 +3,7 @@
     <div class="col-md-12">
       <div class="row">
         <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-6 col-md-6 mb-4">
           <div class="top-card card border-left-primary shadow h-100 py-2">
             <div class="card-body">
               <div class="row no-gutters align-items-center">
@@ -20,7 +20,7 @@
             </div>
           </div>
         </div>
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-6 col-md-6 mb-4">
           <div class="top-card card border-left-primary shadow h-100 py-2">
             <div class="card-body">
               <div class="row no-gutters align-items-center">
